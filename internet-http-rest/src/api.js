fff956e78162b6502eef647fb468622bc1bede63
judithsanchez <judith.sanchez@toptal.com>
2023-10-24 19:30:22 +0200

    Get all api is working

--- internet-http-rest/src/api.js
+++ internet-http-rest/src/api.js
@@ -1,14 +1,1 @@
-const URL = 'http://localhost:3000/tasks';
-
-async function getAllTasks() {
-	try {
-		const response = await fetch(URL);
-		if (!response.ok) {
-			throw new Error('Failed to fetch tasks');
-		}
-		return response.json();
-	} catch (error) {
-		console.error('Error fetching tasks:', error);
-		throw error;
-	}
-}
+const URL = 'http://localhost:3000/';
--- internet-http-rest/src/index.js
+++ internet-http-rest/src/index.js
@@ -1,1 +1,10 @@
-console.log('It is linked');
+const URL = 'http://localhost:3000/tasks';
+
+const renderTasks = async () => {
+	const res = await fetch(URL);
+	const tasks = await res.json();
+	console.log(tasks);
+	return tasks;
+};
+
+window.addEventListener('DOMContentLoaded', () => renderTasks());
