9cb7c0f9119416327cd8ef348345781a2f38d922
judithsanchez <judith.sanchez@toptal.com>
2023-10-24 19:44:15 +0200

    Tasks are rendering on load

--- internet-http-rest/src/index.js
+++ internet-http-rest/src/index.js
@@ -1,9 +1,26 @@
 const URL = 'http://localhost:3000/tasks';
 
+const container = document.querySelector('.tasks');
+
 const renderTasks = async () => {
 	const res = await fetch(URL);
 	const tasks = await res.json();
 	console.log(tasks);
+
+	let template = '';
+
+	tasks.forEach(task => {
+		template += `
+        <div class="task">
+            <h2>${task.title}</h2> 
+            <p>${task.description}</p>
+            <p>${task.done ? 'done' : 'to do'}</p> 
+
+        `;
+	});
+
+	container.innerHTML = template;
+
 	return tasks;
 };
 
