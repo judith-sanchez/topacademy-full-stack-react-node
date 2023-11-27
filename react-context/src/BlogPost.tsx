05050ed5732d88171b402a13b558046b4be01fcb
judithsanchez <judith.sanchez@toptal.com>
2023-11-27 16:55:33 +0100

    Working on design

--- react-context/src/BlogPost.tsx
+++ react-context/src/BlogPost.tsx
@@ -1,49 +1,50 @@
 import React from 'react';
 import blogPosts from './blogPosts.json';
-import { useComments, Comment as CommentType } from './CommentsContext';
+import {useComments, Comment as CommentType} from './CommentsContext';
+import styles from './styles/BlogPost.module.css';
 
 import Comment from './Comment';
 
 interface BlogPost {
-  id: number;
-  title: string;
-  text: {
-    paragraph: string;
-    codeExample?: string;
-  }[];
+	id: number;
+	title: string;
+	text: {
+		paragraph: string;
+		codeExample?: string;
+	}[];
 }
 
 const Blog: React.FC = () => {
-  const blogComments: CommentType[] = useComments();
+	const blogComments: CommentType[] = useComments();
 
-  return (
-    <>
-      <div className="blog-post-container">
-        {blogPosts.map((post: BlogPost) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            {post.text.map((section, index) => (
-              <div key={index}>
-                <p>{section.paragraph}</p>
-                {section.codeExample && <pre>{section.codeExample}</pre>}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      {blogComments?.map((comment) => (
-        <Comment
-          key={comment.id}
-          commentId={comment.id}
-          text={comment.text}
-          username={comment.username}
-          avatar={comment.avatar}
-          datePosted={comment.date}
-          votes={comment.votes}
-        />
-      ))}
-    </>
-  );
+	return (
+		<>
+			<div className={styles.blogPost}>
+				{blogPosts.map((post: BlogPost) => (
+					<div key={post.id}>
+						<h1>{post.title}</h1>
+						{post.text.map((section, index) => (
+							<div key={index}>
+								<p>{section.paragraph}</p>
+								{section.codeExample && <pre>{section.codeExample}</pre>}
+							</div>
+						))}
+					</div>
+				))}
+			</div>
+			{blogComments?.map(comment => (
+				<Comment
+					key={comment.id}
+					commentId={comment.id}
+					text={comment.text}
+					username={comment.username}
+					avatar={comment.avatar}
+					datePosted={comment.date}
+					votes={comment.votes}
+				/>
+			))}
+		</>
+	);
 };
 
 export default Blog;
--- react-context/src/Comment.tsx
+++ react-context/src/Comment.tsx
@@ -1,71 +1,78 @@
 import React from 'react';
-import useComment, { CommentActions } from './useComment';
+import useComment, {CommentActions} from './useComment';
 import styles from './styles/Comment.module.css';
 
 interface CommentProps {
-  commentId: number;
-  datePosted: string;
-  username: string;
-  avatar: string;
-  text: string;
-  votes: number;
+	commentId: number;
+	datePosted: string;
+	username: string;
+	avatar: string;
+	text: string;
+	votes: number;
 }
 
 const Comment: React.FC<CommentProps> = ({
-  commentId,
-  datePosted,
-  username,
-  avatar,
-  text,
-  votes,
+	commentId,
+	datePosted,
+	username,
+	avatar,
+	text,
+	votes,
 }: CommentProps) => {
-  const { addVote, subtractVote, editComment, deleteComment }: CommentActions =
-    useComment(commentId);
+	const {addVote, subtractVote, editComment, deleteComment}: CommentActions =
+		useComment(commentId);
 
-  return (
-    <div className={styles.mainContainer}>
-      <div className={styles.votes}>
-        <button
-          onClick={() => {
-            addVote();
-          }}
-        >
-          👍🏻
-        </button>
-        <p>{votes}</p>
-        <button
-          onClick={() => {
-            subtractVote();
-          }}
-        >
-          👎🏻
-        </button>
-      </div>
-      <div className={styles.secondaryContainer}>
-        <div className={styles.infoAndFunctionalities}>
-          <p>{avatar}</p>
-          <p>{username}</p>
-          <p>{datePosted}</p>
-          <button
-            onClick={() => {
-              editComment();
-            }}
-          >
-            ✏️
-          </button>
-          <button
-            onClick={() => {
-              deleteComment();
-            }}
-          >
-            🗑️
-          </button>
-          <button>💬</button>
-        </div>
-        <p>{text}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.mainContainer}>
+			<div className={styles.votes}>
+				<button
+					onClick={() => {
+						addVote();
+					}}
+				>
+					👍🏻
+				</button>
+				<p>{votes}</p>
+				<button
+					onClick={() => {
+						subtractVote();
+					}}
+				>
+					👎🏻
+				</button>
+			</div>
+			<div className={styles.secondaryContainer}>
+				<div className={styles.infoAndFunctionalities}>
+					<div className={styles.postInfo}>
+						<div className={styles.avatar}>
+							<p>{avatar}</p>
+						</div>
+
+						<p className={styles.username}>{username}</p>
+						<p className={styles.datePosted}>{datePosted}</p>
+					</div>
+					<div className={styles.functionalities}>
+						<button
+							onClick={() => {
+								editComment();
+							}}
+						>
+							✏️
+						</button>
+						<button
+							onClick={() => {
+								deleteComment();
+							}}
+						>
+							🗑️
+						</button>
+						<button className={styles.reply}>↩️ Reply</button>
+					</div>
+				</div>
+				<p>{text}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Comment;
